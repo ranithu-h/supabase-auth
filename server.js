bc521e93b2f353e405226d4d855408677b32e811
rnaithu-h <ranithuhetti10@gmail.com>
2026-08-26 13:30:58 +0200

    Stage 2: public route and unverified protected route

--- server.js
+++ server.js
@@ -48,6 +48,26 @@ app.post('/auth/login', async (req, res) => {
     });
 });
 
+app.get('/public/info', (req, res) => {
+    res.status(200).json({ "message": "Welcome stranger! This info is public." })
+});
+
+app.get('/protected/profile', async (req, res) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")){
+        return res.status(401).json({ error: "Access token required" });
+    }
+
+    const token = authHeader.split(" ")[1]
+
+    if (!token) {
+        return res.status(401).json({ error: "Access token required" });
+    }
+
+    res.status(200).json({ message: "Token received (not yet verified)" });
+});
+
 app.listen(port, () => {
     console.log(`Server running and connected to Supabase on port ${port}`);
 });
