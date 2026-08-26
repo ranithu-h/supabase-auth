4795fcc451bf8a998b4ac526f914633a27783b9d
rnaithu-h <ranithuhetti10@gmail.com>
2026-08-26 15:05:58 +0200

    Stage 3: profile route token verification

--- server.js
+++ server.js
@@ -65,7 +65,15 @@ app.get('/protected/profile', async (req, res) => {
         return res.status(401).json({ error: "Access token required" });
     }
 
-    res.status(200).json({ message: "Token received (not yet verified)" });
+    const { data, error } = await supabase.auth.getUser(token)
+
+    if (error){
+        return res.status(401).json({ "error": "Invalid or expired token" })
+    }
+
+    const user = data.user
+
+    res.status(200).json({ "id": user.id, "email": user.email, "account-created date": user.created_at })
 });
 
 app.listen(port, () => {
