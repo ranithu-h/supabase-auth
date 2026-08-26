5054acad4f4d73011d3c7cf975e5d4f3a7dc3a4a
rnaithu-h <ranithuhetti10@gmail.com>
2026-08-26 13:13:02 +0200

    Stage 1: signup and login routes working

--- server.js
+++ server.js
@@ -8,6 +8,46 @@ const port = process.env.PORT;
 
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY)
 
+app.post('/auth/signup', async (req, res) => {
+    const {email, password} = req.body;
+    
+    if (!email || !password) {
+        return res.status(400).json({ error: "Email and password are required" });
+    }
+
+    const { data, error } = await supabase.auth.signUp({ 
+        email: email, 
+        password: password 
+    });
+
+    if (error) {
+        return res.status(400).json({ error: error.message });
+    }
+
+    res.status(201).json(data.user);
+});
+
+app.post('/auth/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({ error: "Email and password are required" });
+    }
+
+    const { data, error } = await supabase.auth.signInWithPassword({ email, password });
+
+    if (error && error.status === 401) {
+        return res.status(401).json({ error: "Invalid login credentials" });
+    } else if (error) {
+        return res.status(error.status).json({ error: error.message });
+    }
+
+    res.status(200).json({
+        access_token: data.session.access_token,
+        refresh_token: data.session.refresh_token
+    });
+});
+
 app.listen(port, () => {
-  console.log(`Server running and connected to Supabase on port ${port}`);
+    console.log(`Server running and connected to Supabase on port ${port}`);
 });
